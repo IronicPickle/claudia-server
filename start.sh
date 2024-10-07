678626c77599d009895afcf6f11b783eb21989ea
Nathan <nathanrath@live.co.uk>
2024-10-07 15:00:32 +0100

    added db container name option to start script

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-docker start docker-mongo
+
+DB_CONTAINER_NAME=$1
+
+if [ ! $DB_CONTAINER_NAME ]; then
+  DB_CONTAINER_NAME="docker-mongo"
+fi
+
+docker start $DB_CONTAINER_NAME
 deno run "$@" --allow-env --allow-net --allow-read ./src/index.ts 
